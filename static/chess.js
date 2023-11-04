ec6edc69c345ecb82847885bcf6ce309da785b2f
Daniel Chua <danielchua@gmail.com>
2023-11-04 15:02:24 +0800

    Configure and displayc hessboard

--- static/chess.js
+++ static/chess.js
@@ -1,8 +1,16 @@
-const body = document.querySelector('body');
-
+const body = document.querySelector("body");
 
 const gameId = body.dataset.gameid;
 const orientation = body.dataset.orientation;
 
+console.info(`gameId=${gameId}, orientation=${orientation}`);
 
-console.info(`gameId=${gameId}, orientation=${orientation}`)
+// Create a chess configuration
+
+const config = {
+    draggable: true,
+    position: "start",
+    orientation,
+};
+
+var board1 = Chessboard("board1", config);
