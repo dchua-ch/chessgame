b425ec6fa06dafb4f3b5d0a27730f58c201369b6
Daniel Chua <danielchua@gmail.com>
2023-11-04 15:06:32 +0800

    add the ondrop method

--- static/chess.js
+++ static/chess.js
@@ -5,12 +5,17 @@ const orientation = body.dataset.orientation;
 
 console.info(`gameId=${gameId}, orientation=${orientation}`);
 
-// Create a chess configuration
+// Handle onDrop
+const onDrop = (src, dst, piece) => {
+    console.info(`src=${src}, dst=${dst}, piece=${piece}`);
+};
 
+// Create a chess configuration
 const config = {
     draggable: true,
     position: "start",
     orientation,
+    onDrop
 };
 
 var board1 = Chessboard("board1", config);
