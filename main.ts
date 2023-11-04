2ec10b1eb3922fe2c62ea9319e355e9b5d0b722e
Daniel Chua <danielchua@gmail.com>
2023-11-04 13:56:52 +0800

    Create basic express app

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import express from "express";
+import morgan from "morgan";
+import { engine } from "express-handlebars";
+import { v4 as uuidv4 } from "uuid";
+
+// If port is not set, default to 3000
+const port = process.env.PORT || 3000;
+
+// create an instance of ze application
+
+const app = express();
+
+app.get("/", (_req, res) => res.send("Hello world!"));
+
+app.listen(port, () => {
+    console.info(`Application bound to port ${port} at ${new Date()}`);
+});
